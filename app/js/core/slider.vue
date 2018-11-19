d6951782f66718942227c9984d5e2c667b320a3e
NvDsper <2282481418@qq.com>
2018-11-19 09:28:28 +0800

    first commit

---
+++ app/js/core/slider.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+    <section :class="cname">
+        <swiper :options="options" :not-next-tick="options.notNextTick">
+            <swiper-slide>a</swiper-slide>
+        </swiper>
+    </section>
+</template>
+
+<script>
+import {swiper,swiperSlide} from "vue-awesome-swiper";
+export default{
+    components:{
+        swiper,
+        swiperSlide,
+    },
+    props:{
+        options:{
+            type:Object,
+            default(){
+                return{
+                    autoplay:true,
+                    loop:true,
+                    pagination:{
+                        el:".swiper-pagination",
+                    },
+                    notNextTick:false,
+                }
+            },
+        },
+    },
+}    
+</script>
+
+<style lang="css">
+@import "~swiper/dist/css/swiper.css";
+</style>
