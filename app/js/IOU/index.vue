99d23d2f87226a1148c6cf1604672d2eb9a3a67c
NvDsper <2282481418@qq.com>
2018-11-26 15:48:09 +0800

    IOU commit

---
+++ app/js/IOU/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <Header/>
+        <Slider/>
+        <Welfare/>
+        <More-service/>
+        <people-service/>
+        <Footer/>
+        <Navbar/>
+    </div>
+</template>
+<script>
+import Header from "../public/header.vue"
+import Footer from "../public/footer.vue"
+import Navbar from "../public/navbar.vue"
+import Slider from "./Islider.vue"
+import Welfare from "./welfare.vue"
+import MoreService from "./moreService.vue"
+import PeopleService from "./peopleService.vue"
+
+export default {
+    components: {
+        Header,
+        Footer,
+        Navbar,
+        Slider,
+        Welfare,
+        MoreService,
+        PeopleService,
+    },
+}
+</script>
+<style lang="scss" module>
+
+</style>
--- app/js/router/index.js
+++ app/js/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Home from "../home/index.vue"
 import Money from "../money/index.vue"
+import IOU from "../IOU/index.vue"
 import "../../css/reset.scss"
 
 Vue.use(Router)
@@ -17,6 +18,11 @@ export default new Router({
             path: "/money",
             name: "money",
             component: Money,
+        },
+        {
+            path: "/IOU",
+            name: "IOU",
+            component: IOU,
         },
     ],
 })
